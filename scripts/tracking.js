ff2ffa36a7a465f6af30a2e5b63f92e470d8c653
Cillian Myles <mylsey4thewin@gmail.com>
2018-01-25 18:34:11 +0000

    Only track stats in production environment;

---
+++ scripts/tracking.js
@@ -0,0 +1,18 @@
+// Only want to track real traffic and not development page loads!!
+
+// StatCounter tracking function
+function trackViews(){
+    var sc_project=11592789;
+    var sc_invisible=1;
+    var sc_security="05bd10b3";
+    var scJsHost = (("https:" == document.location.protocol) ?
+        "https://secure." : "http://www.");
+    document.write("<sc"+"ript type='text/javascript' src='" 
+        + scJsHost + "statcounter.com/counter/counter.js'></"+"script>");
+}
+
+if (window.location.protocol === 'file:') {
+    console.log('Not tracking when using file: protocol for local dev');
+} else {
+    trackViews();
+}
