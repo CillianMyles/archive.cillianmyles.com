67d5a48428d9e799ac5f382af5a6f2906d27b652
Cillian Myles <mylsey4thewin@gmail.com>
2018-01-31 13:09:09 +0000

    Fixing tracking scripts;

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -2,13 +2,22 @@
 
 // StatCounter tracking function
 function trackViews() {
-    var sc_project = 11592789;
-    var sc_invisible = 1;
-    var sc_security = "05bd10b3";
-    var scJsHost = (("https:" == document.location.protocol) ?
-        "https://secure." : "http://www.");
-    document.write("<sc"+"ript type='text/javascript' src='" 
-        + scJsHost + "statcounter.com/counter/counter.js'></"+"script>");
+
+  // First write the details of the project.
+  document.write('<script type="text/javascript">'
+    + ' var sc_project=11592789;'
+    + ' var sc_invisible=1;'
+    + ' var sc_security="05bd10b3";'
+    + ' var scJsHost = (("https:" == document.location.protocol) ?'
+    + ' "https://secure." : "http://www.");'
+    + '</script>'
+  );
+
+  // Then call the StatCounter script.
+  document.write('<script type="text/javascript" '
+    + 'src="https://www.statcounter.com/counter/counter.js">'
+    + '</script>'
+  );
 }
 
 if (window.location.protocol === 'file:') {
