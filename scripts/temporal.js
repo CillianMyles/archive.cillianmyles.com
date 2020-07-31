c53a343f61520bbb1c4175ea03acd5b860c0c978
Cillian Myles <mylsey4thewin@gmail.com>
2020-07-31 13:17:16 +0100

    Auto update copyright year | abstract to re-usable JS function;

---
+++ scripts/temporal.js
@@ -0,0 +1,30 @@
+
+function printAge() {
+    document.write(age());
+}
+
+function age() {
+    return _age("1993-05-13");
+}
+
+function _age(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+function printCopyrightText() {
+    var text = "This is the new Copyright text for the year " + _currentYear();
+    document.getElementById("copyright-text").innerHTML = text;
+}
+
+function _currentYear() {
+    return new Date().getFullYear();
+}
+
+printCopyrightText();
